8b13050f32f237cce48ef037f20283b15f70f583
panamabilly <billsweeney85@gmail.com>
2022-02-17 11:58:04 -0600

    Update Map.js Dynamic Pin and Popup-card information from backend

--- quest-log/src/Map.js
+++ quest-log/src/Map.js
@@ -1,13 +1,30 @@
 import * as React from 'react';
 import MapGL, { GeolocateControl, Marker, Popup } from 'react-map-gl';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import RoomIcon from '@material-ui/icons/Room';
 import './Map.css';
+import axios from 'axios';
+import { format } from 'timeago.js';
 
 function Map() {
-	function handleClick(event) {
-		console.log(event.lngLat);
-	}
+	const [pins, setPins] = useState([]);
+	const [currentLocation, setCurrentLocation] = useState(null);
+
+	useEffect(() => {
+		const getPins = async () => {
+			try {
+				const res = await axios.get('/pins');
+				setPins(res.data);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		getPins();
+	}, []);
+
+	const handleMarkerClick = (id) => {
+		setCurrentLocation(id);
+	};
 
 	return (
 		<MapGL
@@ -17,37 +34,44 @@ function Map() {
 				zoom: 6.25,
 			}}
 			style={{ width: '95vw', height: '95vh' }}
-			mapStyle='mapbox://styles/panamabilly/ckzpxm04w000p15qd0u7zvdgs'
-			onClick={handleClick}>
+			mapStyle='mapbox://styles/panamabilly/ckzpxm04w000p15qd0u7zvdgs'>
 			<GeolocateControl />
-			<Marker
-				latitude={40.770359}
-				longitude={-111.891869}
-				offsetLeft={-5}
-				offsetTop={-5}
-				draggable={false}>
-				<RoomIcon style={{ color: 'yellow' }} />
-			</Marker>
-			<Popup
-				longitude={-111.891869}
-				latitude={40.770359}
-				offset={15}
-				closeButton={true}
-				anchor='left'>
-				<div className='popup-card'>
-					<label>Place</label>
-					<p className='place-text'>Temple Square</p>
-					<label>Description</label>
-					<p className='description-text'>A Holy Place for Mormons</p>
-					<label>Rating</label>
-					<div className='rating-text'>5 Stars</div>
-					<label>Information</label>
-					<span className='username'>
-						Created by <b>coolboarder1985</b>
-					</span>
-					<span className='date'> 1 minute ago</span>
-				</div>
-			</Popup>
+			{pins.map((p) => (
+				<>
+					<Marker
+						latitude={p.lat}
+						longitude={p.long}
+						offsetLeft={-5}
+						offsetTop={-5}
+						draggable={false}>
+						<RoomIcon
+							style={{ color: 'yellow' }}
+							onClick={() => handleMarkerClick(p._id)}
+						/>
+					</Marker>
+					{p._id === c}
+					<Popup
+						longitude={p.long}
+						latitude={p.lat}
+						offset={15}
+						closeButton={true}
+						anchor='left'>
+						<div className='popup-card'>
+							<label>Place</label>
+							<p className='place-text'>{p.title}</p>
+							<label>Description</label>
+							<p className='description-text'>{p.description}</p>
+							<label>Rating</label>
+							<div className='rating-text'>5 Stars</div>
+							<label>Information</label>
+							<span className='username'>
+								Created by <b>{p.userName}</b>
+							</span>
+							<span className='date'> {format(p.CreatedAt)}</span>
+						</div>
+					</Popup>
+				</>
+			))}
 		</MapGL>
 	);
 }
