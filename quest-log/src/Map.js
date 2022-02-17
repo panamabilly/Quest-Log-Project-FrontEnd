9700adf903f55485f635924b45d9506bfa1fd63d
panamabilly <billsweeney85@gmail.com>
2022-02-17 10:09:51 -0600

    Style Pop-up card in Map.css

--- quest-log/src/Map.js
+++ quest-log/src/Map.js
@@ -1,18 +1,10 @@
 import * as React from 'react';
-import MapGL, { GeolocateControl, Marker } from 'react-map-gl';
+import MapGL, { GeolocateControl, Marker, Popup } from 'react-map-gl';
 import { useState } from 'react';
+import RoomIcon from '@material-ui/icons/Room';
+import './Map.css';
 
 function Map() {
-	const [markers, setMarkers] = useState([]);
-
-	// const handleClick = (event) => {
-	// 	let newMarker = {
-	// 		lon: event.lngLat.lng,
-	// 		lat: event.lngLat.lat,
-	// 	};
-	// 	setMarkers([...markers, newMarker]);
-	// };
-
 	function handleClick(event) {
 		console.log(event.lngLat);
 	}
@@ -28,17 +20,34 @@ function Map() {
 			mapStyle='mapbox://styles/panamabilly/ckzpxm04w000p15qd0u7zvdgs'
 			onClick={handleClick}>
 			<GeolocateControl />
-			{/* {markers.length > 0 &&
-				markers.map((marker) => {
-					<Marker latitude={marker.lat} longitude={marker.lon}>
-						<div
-							style={{
-								width: '10px',
-								height: '10px',
-								backgroundColor: 'red',
-							}}></div>
-					</Marker>;
-				})} */}
+			<Marker
+				latitude={40.770359}
+				longitude={-111.891869}
+				offsetLeft={-5}
+				offsetTop={-5}
+				draggable={false}>
+				<RoomIcon style={{ color: 'yellow' }} />
+			</Marker>
+			<Popup
+				longitude={-111.891869}
+				latitude={40.770359}
+				offset={15}
+				closeButton={true}
+				anchor='left'>
+				<div className='popup-card'>
+					<label>Place</label>
+					<p className='place-text'>Temple Square</p>
+					<label>Description</label>
+					<p className='description-text'>A Holy Place for Mormons</p>
+					<label>Rating</label>
+					<div className='rating-text'>5 Stars</div>
+					<label>Information</label>
+					<span className='username'>
+						Created by <b>coolboarder1985</b>
+					</span>
+					<span className='date'> 1 minute ago</span>
+				</div>
+			</Popup>
 		</MapGL>
 	);
 }
