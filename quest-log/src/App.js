e557835ed293741a644f676bbbfeafdcbcad0e11
panamabilly <billsweeney85@gmail.com>
2022-02-24 13:38:48 -0600

    Add Footer

--- quest-log/src/App.js
+++ quest-log/src/App.js
@@ -1,10 +1,12 @@
 import Map from './Map';
 import Register from './/components/Register';
+import Footer from './components/Footer/Footer';
 
 function App() {
 	return (
 		<div className='App'>
 			<Map />
+			<Footer />
 		</div>
 	);
 }
---
+++ quest-log/src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer(props) {
+	return (
+		<header>
+			<p className='footer-bar'>
+				Quest-Log developed by Bill Sweeney. Contact me @
+				billsweeney85@gmail.com{' '}
+			</p>
+			<p>
+				{' '}
+				To use App double click on any location to drop a pin and save your
+				thoughts about the quests you have done around the world!
+			</p>
+		</header>
+	);
+}
+
+export default Footer;
