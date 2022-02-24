c1df4413e82e8a4c67ad0d5d3f6394a4a7fbb748
panamabilly <billsweeney85@gmail.com>
2022-02-24 12:21:19 -0600

    Remove eslint disable import from app.js

--- quest-log/src/App.js
+++ quest-log/src/App.js
@@ -1,12 +1,5 @@
 import Map from './Map';
 import Register from './/components/Register';
-import mapboxgl from 'mapbox-gl';
-// eslint-disable-next-line import/no-webpack-loader-syntax
-import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
-
-mapboxgl.accessToken =
-	'pk.eyJ1IjoicGFuYW1hYmlsbHkiLCJhIjoiY2wwMDUxa3VyMGdvMjNscWhsdzlsdGg0ZSJ9.cVIiUKTUn0Bb0R2VZczmvQ';
-mapboxgl.workerClass = MapboxWorker;
 
 function App() {
 	return (
