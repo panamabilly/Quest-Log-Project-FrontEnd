5a7372eca6c0f4cfc187486ab5ddb6f830fc3660
panamabilly <billsweeney85@gmail.com>
2022-02-24 14:29:16 -0600

    Add Header Instructions

--- quest-log/src/components/Footer/Footer.js
+++ quest-log/src/components/Footer/Footer.js
@@ -7,11 +7,6 @@ function Footer(props) {
 				Quest-Log developed by Bill Sweeney. Contact me @
 				billsweeney85@gmail.com{' '}
 			</p>
-			<p>
-				{' '}
-				To use App double click on any location to drop a pin and save your
-				thoughts about the quests you have done around the world!
-			</p>
 		</header>
 	);
 }
